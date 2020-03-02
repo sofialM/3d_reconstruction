87f80b1ea42dff01f2fb08da96e2d281262b25e9
sofialM <61688482+sofialM@users.noreply.github.com>
2020-03-02 10:44:50 +0000

    Create sqrfunction.py

---
+++ sqrfunction.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  2 10:38:15 2020
+
+@author: sofia
+"""
+
+def sqr(a):
+    b=[]
+    for i in range (len(a)):
+        b=b+[a[i]**2]
+    return b
+    
